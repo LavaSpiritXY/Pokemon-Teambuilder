2b91703e6e8492163c3ceb7371e21cabeaa84de0
Nikhil Eaga <Nikkyboy105@gmail.com>
2026-08-13 10:51:04 +0100

    Update 20: extract Champions configuration constants

--- app.py
+++ app.py
@@ -7,7 +7,13 @@ from typing import Dict, List, Set, Tuple
 import requests
 import streamlit as strlit
 import pandas as pd
-from champions.constants import CURRENT_REGULATION
+from champions.constants import (
+    CURRENT_REGULATION,
+    TYPE_COLORS,
+    TYPE_SVG_URLS,
+    NATURES,
+    SPECIES_DISPLAY_OVERRIDES,
+)
 from champions_phase18_5 import render_champions_profile_v6
 from champions_phase18_6_ui import render_dynamic_stat_controls, render_dynamic_stat_graph
 from champions_phase18 import render_champions_profile_v3
@@ -884,120 +890,6 @@ strlit.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-TYPE_COLORS = {
-    "Normal": "#A8A77A", "Fire": "#EE8130", "Water": "#6390F0", "Electric": "#F7D02C",
-    "Grass": "#7AC74C", "Ice": "#96D9D6", "Fighting": "#C22E28", "Poison": "#A33EA1",
-    "Ground": "#E2BF65", "Flying": "#A98FF3", "Psychic": "#F95587", "Bug": "#A6B91A",
-    "Rock": "#B6A136", "Ghost": "#735797", "Dragon": "#6F35FC", "Dark": "#705746",
-    "Steel": "#B7B7CE", "Fairy": "#D685AD"
-}
-
-TYPE_SVG_URLS = {
-    "Normal": "https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/normal.svg",
-    "Fire": "https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/fire.svg",
-    "Water": "https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/water.svg",
-    "Electric": "https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/electric.svg",
-    "Grass": "https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/grass.svg",
-    "Ice": "https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/ice.svg",
-    "Fighting": "https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/fighting.svg",
-    "Poison": "https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/poison.svg",
-    "Ground": "https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/ground.svg",
-    "Flying": "https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/flying.svg",
-    "Psychic": "https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/psychic.svg",
-    "Bug": "https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/bug.svg",
-    "Rock": "https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/rock.svg",
-    "Ghost": "https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/ghost.svg",
-    "Dragon": "https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/dragon.svg",
-    "Dark": "https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/dark.svg",
-    "Steel": "https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/steel.svg",
-    "Fairy": "https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/fairy.svg"
-}
-
-NATURES = [
-    "Adamant (+Atk, -SpA)", "Bold (+Def, -Atk)", "Brave (+Atk, -Spe)", "Calm (+SpD, -Atk)",
-    "Careful (+SpD, -SpA)", "Gentle (+SpD, -Def)", "Hardy", "Hasty (+Spe, -Def)",
-    "Impish (+Def, -SpA)", "Jolly (+Spe, -SpA)", "Lax (+Def, -SpD)", "Lonely (+Atk, -Def)",
-    "Mild (+SpA, -Def)", "Modest (+SpA, -Atk)", "Naive (+Spe, -SpD)", "Naughty (+Atk, -SpD)",
-    "Quiet (+SpA, -Spe)", "Rash (+SpA, -SpD)", "Relaxed (+Def, -Spe)", "Sassy (+SpD, -Spe)",
-    "Serious", "Timid (+Spe, -Atk)"
-]
-
-SPECIES_DISPLAY_OVERRIDES = {
-
-    # ==========================================
-    # PALDEA FORMS (Squashed Fallbacks for UI)
-    # ==========================================
-    "taurospaldeacombat": "Tauros Paldea Combat",
-    "taurospaldeacombatbreed": "Tauros Paldea Combat",
-    "taurospaldeablaze": "Tauros Paldea Blaze",
-    "taurospaldeaaqua": "Tauros Paldea Aqua",
-
-    # ==========================================
-    # PALDEA FORMS (Clean Endpoint Matches)
-    # ==========================================
-    "tauros-paldea-combat-breed": "Tauros Paldea Combat",
-    "tauros-paldea-blaze": "Tauros Paldea Blaze",
-    "tauros-paldea-aqua": "Tauros Paldea Aqua",
-
-    # =========================================================
-    # BASCULEGION
-    # =========================================================
-
-    "basculegion-male": "Basculegion",
-    "basculegion-m": "Basculegion",
-    "basculegionm": "Basculegion",
-    "basculegion-f": "Basculegion Female",
-    "basculegion-female": "Basculegion Female",
-    "basculegionf": "Basculegion Female",
-
-    # =========================================================
-    # INDEEDEE
-    # =========================================================
-
-    "indeedeef": "Indeedee Female",
-    "indeedee-f": "Indeedee Female",
-    "indeedee-female": "Indeedee Female",
-
-    "indeedeem": "Indeedee Male",
-    "indeedee-m": "Indeedee Male",
-    "indeedee-male": "Indeedee Male",
-
-    # =========================================================
-    # MEOWSTIC
-    # =========================================================
-
-    "meowsticmale": "Meowstic Male",
-    "meowstic-m": "Meowstic Male",
-    "meowstic-male": "Meowstic Male",
-
-    "meowsticfemale": "Meowstic Female",
-    "meowstic-f": "Meowstic Female",
-    "meowstic-female": "Meowstic Female",
-
-    # =========================================================
-    # OINKOLOGNE
-    # =========================================================
-
-    "oinkolognem": "Oinkologne Male",
-    "oinkologne-m": "Oinkologne Male",
-
-    "oinkolognef": "Oinkologne Female",
-    "oinkologne-f": "Oinkologne Female",
-
-    # =========================================================
-    # EXISTING SPECIAL NAMES
-    # =========================================================
-
-    "mr-mime": "Mr. Mime",
-    "mime-jr": "Mime Jr.",
-    "ho-oh": "Ho-Oh",
-    "nidoran-f": "Nidoran Female",
-    "nidoran-m": "Nidoran Male",
-    "farfetchd": "Farfetch'd",
-    "sirfetchd": "Sirfetch'd",
-    "flabebe": "Flabébé",
-    "type-null": "Type: Null",
-}
 
 strlit.markdown("""
 <style>
